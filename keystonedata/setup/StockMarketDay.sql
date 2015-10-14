8cd6051bbec8db227d8394f1bcca3f50307fc818
luanchuanmalu <jian_hua_yang@163.com>
2015-10-14 12:21:36 +0800

    day and 60m

--- keystonedata/setup/StockMarketDay.sql
+++ keystonedata/setup/StockMarketDay.sql
@@ -1,16 +1,21 @@
 ﻿
 -- create table marketay
-CREATE TABLE marketay
+CREATE TABLE stockmarketday
 (
+  pre_close 	double precision,
   open 		double precision,
   high 		double precision,
   low 		double precision,
   close 	double precision,
+  volume 	double precision,
+  amt 		double precision,
+  --chg 	  double precision,
+  pct_chg 	double precision,
+  adjfactor 	double precision,
   stockid 	text,
   marketdate 	timestamp without time zone,
   wind_code 	text,
+  lastupdate	timestamp without time zone DEFAULT now(),
   PRIMARY KEY 	(stockid, marketdate)
 );
---ALTER TABLE table_name ALTER COLUMN column_name SET DEFAULT default_value;
-ALTER TABLE marketay  OWNER TO keystone;
-
+ALTER TABLE stockmarketday  OWNER TO keystone;
--- keystonedata/setup/StockMarketMin60.sql
+++ keystonedata/setup/StockMarketMin60.sql
@@ -6,10 +6,14 @@ CREATE TABLE marketmin60
   high 		double precision,
   low 		double precision,
   close 	double precision,
+  volume 	double precision,
+  amt 		double precision,
+  chg 	  double precision,
+  pct_chg 	double precision,
   stockid 	text,
   marketdate 	timestamp without time zone,
   wind_code 	text,
+  lastupdate	timestamp without time zone DEFAULT now(),
   PRIMARY KEY 	(stockid, marketdate)
 );
 ALTER TABLE marketmin60  OWNER TO keystone;
-
